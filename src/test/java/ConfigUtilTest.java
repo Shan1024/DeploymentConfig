455b92e627c1a9c53711992fa1dfd577554fdea7
shan1024 <gambit1024@gmail.com>
2016-06-09 18:29:38 +0530

    New tests added

--- src/test/java/ConfigUtilTest.java
+++ src/test/java/ConfigUtilTest.java
@@ -13,20 +13,9 @@ public class ConfigUtilTest {
 
     @Test
     public void getConfigYaml() {
-
         File file = new File("carbon.yml");
         YAML configYaml = ConfigUtil.getConfig(file, YAML.class);
         System.out.println("New YML: \n" + configYaml.getValue());
-
-        FileWriter fileWriter;
-        try {
-            fileWriter = new FileWriter(new File("carbon_new.yml"));
-            fileWriter.write(configYaml.getValue());
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
 
     }
 
@@ -38,46 +27,21 @@ public class ConfigUtilTest {
             FileInputStream fileInputStream = new FileInputStream(file);
             XML configXml = ConfigUtil.getConfig(fileInputStream, file.getName(), XML.class);
             System.out.println("New XML: \n" + configXml.getValue());
-
-            FileWriter fileWriter;
-            try {
-                fileWriter = new FileWriter(new File("log4j_new.xml"));
-                fileWriter.write(configXml.getValue());
-                fileWriter.flush();
-                fileWriter.close();
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
-
     }
 
     @Test
     public void getConfigProperties() {
-
         File file = new File("launch.properties");
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             Properties configProperties = ConfigUtil.getConfig(fileInputStream, file.getName(), Properties.class);
             System.out.println("New Properties: \n" + configProperties);
-
-            FileWriter fileWriter;
-            try {
-                fileWriter = new FileWriter(new File("launch_new.properties"));
-                fileWriter.write(configProperties.getValue());
-                fileWriter.flush();
-                fileWriter.close();
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
-
     }
 
     @Test
